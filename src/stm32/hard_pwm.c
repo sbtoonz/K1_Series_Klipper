eae3ea7280ae72eafe3bac8b4fc6a7659396505c
Alex Voinea <voinea.dragos.alexandru@gmail.com>
2023-01-03 19:43:46 +0200

    stm32: Expand stm32g0 family (#5838)
    Add stm32g07x family support.
    
    Signed-off-by: Alex Voinea <voinea.dragos.alexandru@gmail.com>

--- src/stm32/hard_pwm.c
+++ src/stm32/hard_pwm.c
@@ -117,7 +117,7 @@ static const struct gpio_pwm_info pwm_regs[] = {
     {TIM1,  GPIO('B',  3), 2, GPIO_FUNCTION(1)},
     {TIM3,  GPIO('B',  4), 1, GPIO_FUNCTION(1)},
     {TIM3,  GPIO('B',  5), 2, GPIO_FUNCTION(1)},
-#ifndef CONFIG_MACH_STM32G071
+#if CONFIG_MACH_STM32G0Bx
     {TIM4,  GPIO('B',  6), 1, GPIO_FUNCTION(9)},
     {TIM4,  GPIO('B',  7), 2, GPIO_FUNCTION(9)},
     {TIM4,  GPIO('B',  8), 3, GPIO_FUNCTION(9)},
@@ -136,7 +136,7 @@ static const struct gpio_pwm_info pwm_regs[] = {
     {TIM14, GPIO('C', 12), 1, GPIO_FUNCTION(2)},
     {TIM16, GPIO('D',  0), 1, GPIO_FUNCTION(2)},
     {TIM17, GPIO('D',  1), 1, GPIO_FUNCTION(2)},
-#ifndef CONFIG_MACH_STM32G071
+#if CONFIG_MACH_STM32G0Bx
     {TIM4,  GPIO('D', 12), 1, GPIO_FUNCTION(2)},
     {TIM4,  GPIO('D', 13), 2, GPIO_FUNCTION(2)},
     {TIM4,  GPIO('D', 14), 3, GPIO_FUNCTION(2)},
